8d1f301d8600e88a57cc8a272123685c9152be4b
huaihuai132 <1606132907@qq.com>
2025-10-05 23:50:15 +0800

    feat 文件提交

--- openai-code-review-test/src/test/java/plus/gaga/middleware/test/ApiTest.java
+++ openai-code-review-test/src/test/java/plus/gaga/middleware/test/ApiTest.java
@@ -5,15 +5,16 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import plus.gaga.middleware.Application;
 
 @Slf4j
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = Application.class)
 public class ApiTest {
 
     @Test
     public void test() {
-        System.out.println(Integer.parseInt("123411"));
+        System.out.println(Integer.parseInt("1234111"));
     }
 
 }
