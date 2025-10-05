5ac68fc39a151ae73d27de65f641c058b50e2675
huaihuai132 <1606132907@qq.com>
2025-10-06 00:09:34 +0800

    feat 文件提交

--- openai-code-review-test/src/test/java/plus/gaga/middleware/test/ApiTest.java
+++ openai-code-review-test/src/test/java/plus/gaga/middleware/test/ApiTest.java
@@ -14,7 +14,7 @@ public class ApiTest {
 
     @Test
     public void test() {
-        System.out.println(Integer.parseInt("1234111"));
+        System.out.println(Integer.parseInt("123411"));
     }
 
 }
